ea7e1f41dc84964105289368acc4cb6f3bcf529f
0pripyat92 <devjr0000@gmail.com>
2023-10-05 20:58:11 -0300

    atualização geral 3

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,15 @@
-const jump = () {
+const mario =document.querySelector('.mario');
 
+const jump = () => {
+mario.classList.add('jump');
+
+setTimeout(() => {
+    mario.classList.remove('jump');
+},500);
 }
+const loop =setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+ console.log(pipePosition);
+ if(pipePosition </=120)
+}, 10);
 document.addEventListener('keydown', jump);
